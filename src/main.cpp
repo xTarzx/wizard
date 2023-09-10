3b310c155e489e88d7633942ed030948912c5813
xtarzx <telmomartins2000@gmail.com>
2023-09-10 23:28:43 +0100

    sleep time

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,18 @@ int main(int argc, char const *argv[])
         UDPSocket s;
 
         json_t *root = json_object();
-        json_object_set_new(root, "method", json_string("getDevInfo"));
+        // json_object_set_new(root, "method", json_string("getDevInfo"));
+        json_object_set_new(root, "method", json_string("setPilot"));
+
+        json_t *data = json_object();
+        json_object_set_new(data, "dimming", json_integer(20));
+        json_object_set_new(data, "r", json_integer(0));
+        json_object_set_new(data, "g", json_integer(0));
+        json_object_set_new(data, "b", json_integer(255));
+        json_object_set_new(root, "params", data);
 
         std::string msg = json_dumps(root, JSON_COMPACT);
-        std::string res = s.SendMessage(msg, "192.168.1.255", WIZ_PORT);
+        std::string res = s.SendMessage(msg, "192.168.1.69", WIZ_PORT);
         json_decref(root);
 
         std::cout << res << std::endl;
