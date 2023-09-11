1171b15e2b1d637b15d3643c55641c9075e54f57
xTarzx <telmomartins2000@gmail.com>
2023-09-11 14:05:53 +0100

    imgui test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,65 @@
 
-#include "wizard.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+
+// #include "wizard.h"
+
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
 
 int main(int argc, char const *argv[])
 {
+
+        if (!glfwInit())
+                return 1;
+
+        glfwWindowHint(GLFW_RESIZABLE, false);
+
+        GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "WiZard", NULL, NULL);
+        if (!window)
+        {
+                glfwTerminate();
+                return 1;
+        }
+
+        glfwMakeContextCurrent(window);
+
+        ImGui::CreateContext();
+        ImGuiIO &io = ImGui::GetIO();
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        io.ConfigDockingWithShift = true;
+        ImGui::StyleColorsDark();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init("#version 130");
+
+        while (!glfwWindowShouldClose(window))
+        {
+
+                glfwPollEvents();
+
+                ImGui_ImplOpenGL3_NewFrame();
+                ImGui_ImplGlfw_NewFrame();
+                ImGui::NewFrame();
+
+                ImGui::Render();
+
+                int display_w, display_h;
+                glfwGetFramebufferSize(window, &display_w, &display_h);
+                glViewport(0, 0, display_w, display_h);
+                glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+                glClear(GL_COLOR_BUFFER_BIT);
+                ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+                glfwSwapBuffers(window);
+        }
+
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
 
         return 0;
 }
